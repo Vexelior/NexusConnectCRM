dbb1d66804ef69a3e9b34465f6ce4b90f2f1aa31
Vexelior <67873187+Vexelior@users.noreply.github.com>
2023-08-18 18:32:04 -0700

    add employee and admin layouts in index

--- wwwroot/js/AdminScripts.js
+++ wwwroot/js/AdminScripts.js
@@ -1,1 +1,22 @@
-﻿
+﻿var ctx = document.getElementById('salesChart').getContext('2d');
+var salesChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+        datasets: [{
+            label: 'Sales',
+            data: [15000, 18000, 22000, 21000, 25000, 28000],
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+            fill: true
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
--- wwwroot/js/EmployeeScripts.js
+++ wwwroot/js/EmployeeScripts.js
@@ -1,1 +1,11 @@
-﻿
+﻿var ctx = document.getElementById('taskProgressChart').getContext('2d');
+var taskProgressChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Completed', 'Pending'],
+        datasets: [{
+            data: [60, 40],
+            backgroundColor: ['rgba(75, 192, 192, 0.8)', 'rgba(255, 99, 132, 0.8)']
+        }]
+    }
+});
