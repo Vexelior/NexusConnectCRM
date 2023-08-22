b46a5d63682d379cdb83079593bbcd7cf283937d
Vexelior <67873187+Vexelior@users.noreply.github.com>
2023-08-21 18:19:24 -0700

    add unit testing

---
+++ NexusConnectCRM.Tests/HomeControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NexusConnectCRM.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NexusConnectCRM.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_ReturnsAViewResult()
+        {
+            // Arrange
+            var logger = new Mock<ILogger<HomeController>>();
+            var controller = new HomeController(logger.Object);
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        } 
+    }
+}
