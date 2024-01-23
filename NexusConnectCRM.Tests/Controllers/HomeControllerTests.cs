ca8332899978a398a8837a3a27a02abf2e29561e
Alexander Sanderson <asanderson1994s@gmail.com>
2024-01-22 17:38:51 -0800

    Update HomeControllerTests.cs

--- NexusConnectCRM.Tests/Controllers/HomeControllerTests.cs
+++ NexusConnectCRM.Tests/Controllers/HomeControllerTests.cs
@@ -1,13 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NexusConnectCRM.Controllers;
-using NexusConnectCRM.ViewModels;
-using System.Diagnostics;
 using NUnit.Framework;
 using Moq;
 using System.Threading.Tasks;
-
 
 namespace NexusConnectCRM.Tests.Controllers
 {
